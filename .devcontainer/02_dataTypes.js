d71d284a246ad374031a642a09e514fde5e165d1
Navneet saini <111631034+Navneet-saini9336@users.noreply.github.com>
2025-08-10 19:55:37 +0000

    data type

--- .devcontainer/02_dataTypes.js
+++ .devcontainer/02_dataTypes.js
@@ -1,3 +1,34 @@
-let score = 33
- console.log(typeof score);
- console.log(typeof(score));
+//let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+//
+//let valueInNumber = Number(score)
+//console.log(typeof score);
+//console.log(valueInNumber);
+//  
+//let scored = null
+// console.log(typeof score);
+// console.log(typeof(score));
+//
+//let scoreds = undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+// 
+ // "33" => 33
+ // "33abc" => NaN
+ // true =>1; false => 0
+
+ let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+ //1 => true; 0=> false
+ //"" => false 
+ //"hitesh" => true
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
