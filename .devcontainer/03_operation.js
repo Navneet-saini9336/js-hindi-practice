1fff14d4c5cefc588d4cc2a29dac717733878ada
Navneet saini <111631034+Navneet-saini9336@users.noreply.github.com>
2025-08-13 01:31:35 +0000

    operation

---
+++ .devcontainer/03_operation.js
@@ -0,0 +1,27 @@
+//****************************operation******************************* */
+let value =3
+let neagValue = -value
+
+console.log(neagValue);
+
+let str1 = "hitesh"
+let str2 ="hello"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 +"2");
+
+console.log(1 + 2 + "3");
+
+console.log(+true);
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+
+gameCounter++;
+
+console.log(gameCounter);
