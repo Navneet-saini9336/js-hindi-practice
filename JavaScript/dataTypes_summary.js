52d8f377f9ddfe895adb1fb02d2ee4e1f110954f
Navneet saini <111631034+Navneet-saini9336@users.noreply.github.com>
2025-08-17 06:59:08 +0000

    data type

--- JavaScript/dataTypes_summary.js
+++ JavaScript/dataTypes_summary.js
@@ -1,16 +1,41 @@
- // primitive  = 7
-    String
-    Number
-    Boolean
-    null
-    undefined
-    Symbol
-    BigInt
-    
-    const score = 100
+ // //primitive  = 7
+    //String
+    //Number
+    //Boolean
+    //null
+    //undefined
+    //Symbol
+    //BigInt
+    //
+    //const score = 100
+//
+    //const scoreValue =100.3
+//
+    //const isLoggedIn =false
+    //const outsideTemp =null
+    //let userEmail
+//
+    //const id = Symbol('123')
+    //const anotherId = Symbol('123')
+//
+    //console.log(id === anotherId);
+//
+    //const bigNumber = 34343444344
 
  //Non primitive (Reference)
-    Array
-    Objects
-    Functions
+    //Array 
+    //Objects
+    //Functions
 
+   const heros =[" shaktiman" , "naagraj" , "doga"]
+   let myObj= {
+     name : "hitesh",
+     age : 22, 
+
+   }
+
+  function myFunction() {
+  return "Hello";
+}
+
+console.log(typeof myFunction); // "function"
