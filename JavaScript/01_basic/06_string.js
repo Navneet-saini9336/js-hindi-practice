6cc31686b6409634f9fe289ecd6bf8ebf1a31f4e
Navneet saini <111631034+Navneet-saini9336@users.noreply.github.com>
2025-08-17 21:04:23 +0000

    string

---
+++ JavaScript/01_basic/06_string.js
@@ -0,0 +1,15 @@
+const numae = " hitesh "
+constrepoConst = 50
+
+// String --------------------.
+const gameName = new String('hiteshhc')
+
+console.log(gameName[0]);
+//....................
+console.log(gameName[1]);
+console.log(gameName.__proto__);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName
