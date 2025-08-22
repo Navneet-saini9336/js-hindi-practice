bb789ff2205c45bc4e13c04b1bfce5af8d80b1ba
Navneet saini <111631034+Navneet-saini9336@users.noreply.github.com>
2025-08-22 00:34:15 +0000

    update

---
+++ JavaScript/02_basic/03_object1.js
@@ -0,0 +1,17 @@
+// singleton
+//---object.create
+
+// object literals
+
+const JsUser = {
+    name : "Hitesh",
+    age = 18 , 
+    location : "Jaipur",
+    email: "jitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays:["monday", "saturday"]
+}
+
+
+console.log(JsUser.email)
+console.log(jsUser["email"])
