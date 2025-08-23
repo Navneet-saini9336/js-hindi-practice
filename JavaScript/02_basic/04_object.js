4829c7e090037c612e9826d0d9b371c359b6d1f0
Navneet saini <111631034+Navneet-saini9336@users.noreply.github.com>
2025-08-23 21:48:57 +0000

    object_update

--- JavaScript/02_basic/04_object.js
+++ JavaScript/02_basic/04_object.js
@@ -33,14 +33,50 @@ const obj2 = { 3: "a", 4: "b" };
 
 // Merging obj1 and obj2 into a new object
 // ✅ Important: "Object" is case-sensitive, use "Object.assign", not "object.assign"
-const obj3 = Object.assign({}, obj1, obj2);
+// Using Object.assign
+const obj4 = Object.assign({}, obj1, obj2);
+console.log(obj4); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
-console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// Using spread operator (cleaner way)
+const obj5 = { ...obj1, ...obj2 };
+console.log(obj5); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
-/*
-Explanation:
+
+const users = [
+      
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+
+    },
+    {
+
+    },
+ 
+
+
+]
+
+users[1].email
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+
+
+
+
+
+/*Explanation:
 - Object.assign(target, ...sources)
   → Copies all properties from source objects into the target.
 - If you pass obj1 as target → obj1 will be modified.
 - Passing {} as target → creates a new object, keeping obj1 & obj2 unchanged.
 */
+
